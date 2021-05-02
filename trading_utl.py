2fdc400d0fa5ac692c4004273e9906bea22481d9
Kyle O'Connor <koconnor5190@gmail.com>
2021-05-02 14:04:16 -0700

    doc: header docstring

---
+++ trading_utl.py
@@ -0,0 +1,19 @@
+"""
+file: trading_utl.py
+description: utilities for accessing API for algotrading
+created by: Kyle O'Connor
+created: 2021-05-02
+last modified: 2021-05-02
+"""
+
+# imports
+import datetime as dt
+import requests
+
+class AlphaVantageAPI(requests.Session):
+
+    def __init__(self, api_key):
+        super(AlphaVantageAPI, self).__init__
+
+        
+
