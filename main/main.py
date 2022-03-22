633908ad4f0b428546d561e4cdc185307eed1fc0
Kyle O'Connor <koconnor5190@gmail.com>
2022-03-21 21:05:29 -0700

    updated scripts

--- main/main.py
+++ main/main.py
@@ -7,7 +7,7 @@ last modified: 2021-05-03
 """
 import inspect
 import os
-import pathlib
+import pandas as pd
 import sys
 
 SCRIPT_PTH = os.path.realpath(inspect.stack()[0][1])
@@ -18,23 +18,33 @@ sys.path.insert(0, SCRIPT_LOC)
 sys.path.insert(0, PARENT_DIR)
 from utilities.trading_utl import *
 api_keys_dir = os.path.join(PARENT_DIR, 'keys')
+x=1
 
 def main():
 
+    # data location
+    data_loc = os.path.join(PARENT_DIR, 'docs', 'NYSE.txt')
+
     # directory for vantage key
     vantage_keys = os.path.join(api_keys_dir, 'vantage_key.txt')
+    with open(vantage_keys, 'r') as f:
+        v_key = f.readline()
 
     # get vantage key
     with open(vantage_keys, 'r') as f:
         vantage_key = f.readline().strip().split()[0]
 
-    url_params = {
-        'function': 'TIME_SERIES_INTRADAY', 'symbol': 'IBM',
-        'interval': '5min', 'apikey': vantage_key
-    }
-    vantage_api_conn = AlphaVantageAPI(vantage_key)
-    check = vantage_api_conn.get_time_series(url_params)
-    
+    vantage_api_conn = AlphaVantageAPI(v_key)
+    syms, desc = vantage_api_conn.get_nyse_symbols(data_loc)
+
+    data = {}
+
+    for s in syms:
+        l = vantage_api_conn.get_ema(s, period='13')
+        sh = vantage_api_conn.get_ema(s, period='48')
+        data[s] = {}
+        data[s]['long_ema'] = l
+        data[s]['short_ema'] = sh
 
 if __name__ == '__main__':
     main()
--- utilities/trading_utl.py
+++ utilities/trading_utl.py
@@ -15,18 +15,46 @@ class AlphaVantageAPI(requests.Session):
 
     def __init__(self, api_key):
         super(AlphaVantageAPI, self).__init__()
+        self.key = api_key
         self.endpoint = 'https://www.alphavantage.co/query?'
 
-    def get_time_series(self, url_params):
+    @staticmethod
+    def get_nyse_symbols(data_loc):
 
-        path = (
-            f'{self.endpoint}{"&".join(f"{key}={value}" for key, value in url_params.items())}'
-        )
-        
-        results = self.get(path)
-        content = json.loads(results.content)
+        stock_symbols = []
+        stock_desc = {}
+        with open(data_loc, 'r') as file:
+            for line in enumerate(file.readlines()):
+                if line[0] == 0:
+                    pass
+                else:
+                    l = line[1].strip().split('\t')
+                    if len(l) == 2:
+                        sym = l[0]
+                        desc = l[1]
+                        stock_symbols.append(sym)
+                        stock_desc[sym] = desc
+                    elif len(l) == 1:
+                        sym = l[0]
+                        stock_symbols.append(sym)
+                        stock_desc[sym] = 'No description'
+                    else:
+                        print('Not sure why this is blank')
+                        pass
 
-        return content
+        return stock_symbols, stock_desc
+
+    def get_ema(self, sym, period):
+
+        url = f'{self.endpoint}function=EMA&symbol={sym}&interval=daily&' \
+              f'time_period={period}&series_type=open&apikey={self.key}'
+        response = requests.get(url)
+        data = response.json()
+
+        return data
+
+
+
 
 
         
