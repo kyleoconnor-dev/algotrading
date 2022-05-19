44a7fb9d0250658d04a4e5a160e777c9816114c7
kyleoconnor-dev <koconnor5190@gmail.com>
2022-05-18 21:23:01 -0700

    added EMA calcs

--- main/main.py
+++ main/main.py
@@ -6,8 +6,10 @@ created: 2021-05-03
 last modified: 2021-05-03
 """
 
+import datetime as dt
 import inspect
 import os
+import pandas as pd
 import sys
 import yahoo_fin.stock_info as si
 
@@ -21,15 +23,29 @@ sys.path.insert(0, PARENT_DIR)
 stock_sym_loc = os.path.join(PARENT_DIR, 'docs', 'NYSE.txt')
 
 def main():
+    start = dt.datetime.now()
+    count = 0
     with open(stock_sym_loc, 'r') as f:
         for line in f.readlines():
             sym = line.strip().split('\t')[0]
             try:
                 data = si.get_data(sym)
+                count += 1
                 # get 13 and 48.5 day moving average
-                x=1
+                data['13_DAY_EMA'] = data['close'].ewm(span=13).mean()
+                data['48_DAY_EMA'] = data['close'].ewm(span=48.5).mean()
+                data['50_DAY_EMA'] = data['close'].ewm(span=48.5).mean()
+                data['200_DAY_EMA'] = data['close'].ewm(span=48.5).mean()
+                fig = data.plot(x='index', y=['13_DAY_EMA'])
             except AssertionError:
                 print('No data found')
+                count += 1
+            except KeyError:
+                print('Random Key Error - skip')
+                count += 1
+    end = dt.datetime.now()
+    total = end - start
+    print(total)
 
 if __name__ == '__main__':
     main()
