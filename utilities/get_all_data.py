57ed461e94e1362473ee736a29cef12de7f29e4a
kyleoconnor-dev <koconnor5190@gmail.com>
2022-05-18 20:32:08 -0700

    removed all the AplhaAdvantage, using yahoo_fin webscraper

---
+++ utilities/get_all_data.py
@@ -0,0 +1,3 @@
+import yahoo_fin.stock_info as yf
+data = yf.get_quote_table('MSFT')
+x=1
--- utilities/trading_utl.py
+++ utilities/trading_utl.py
@@ -7,51 +7,9 @@ last modified: 2021-05-02
 """
 
 # imports
-import datetime as dt
-import json
-import requests
+import yahoo_fin.stock_info as si
 
-class AlphaVantageAPI(requests.Session):
 
-    def __init__(self, api_key):
-        super(AlphaVantageAPI, self).__init__()
-        self.key = api_key
-        self.endpoint = 'https://www.alphavantage.co/query?'
-
-    @staticmethod
-    def get_nyse_symbols(data_loc):
-
-        stock_symbols = []
-        stock_desc = {}
-        with open(data_loc, 'r') as file:
-            for line in enumerate(file.readlines()):
-                if line[0] == 0:
-                    pass
-                else:
-                    l = line[1].strip().split('\t')
-                    if len(l) == 2:
-                        sym = l[0]
-                        desc = l[1]
-                        stock_symbols.append(sym)
-                        stock_desc[sym] = desc
-                    elif len(l) == 1:
-                        sym = l[0]
-                        stock_symbols.append(sym)
-                        stock_desc[sym] = 'No description'
-                    else:
-                        print('Not sure why this is blank')
-                        pass
-
-        return stock_symbols, stock_desc
-
-    def get_ema(self, sym, period):
-
-        url = f'{self.endpoint}function=EMA&symbol={sym}&interval=daily&' \
-              f'time_period={period}&series_type=open&apikey={self.key}'
-        response = requests.get(url)
-        data = response.json()
-
-        return data
 
 
 
