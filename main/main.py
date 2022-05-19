c5eed2942ebb8c54db4b49346a8fa6608464544d
kyleoconnor-dev <koconnor5190@gmail.com>
2022-05-18 20:49:46 -0700

    only need 1 script since yahoo_fin does everything

--- main/main.py
+++ main/main.py
@@ -5,10 +5,11 @@ created by: Kyle O'Connor
 created: 2021-05-03
 last modified: 2021-05-03
 """
+
 import inspect
 import os
-import pandas as pd
 import sys
+import yahoo_fin.stock_info as si
 
 SCRIPT_PTH = os.path.realpath(inspect.stack()[0][1])
 SCRIPT_LOC = os.path.dirname(SCRIPT_PTH)
@@ -16,35 +17,19 @@ PARENT_DIR = os.path.dirname(SCRIPT_LOC)
 sys.path.insert(0, SCRIPT_PTH)
 sys.path.insert(0, SCRIPT_LOC)
 sys.path.insert(0, PARENT_DIR)
-from utilities.trading_utl import *
-api_keys_dir = os.path.join(PARENT_DIR, 'keys')
-x=1
+
+stock_sym_loc = os.path.join(PARENT_DIR, 'docs', 'NYSE.txt')
 
 def main():
-
-    # data location
-    data_loc = os.path.join(PARENT_DIR, 'docs', 'NYSE.txt')
-
-    # directory for vantage key
-    vantage_keys = os.path.join(api_keys_dir, 'vantage_key.txt')
-    with open(vantage_keys, 'r') as f:
-        v_key = f.readline()
-
-    # get vantage key
-    with open(vantage_keys, 'r') as f:
-        vantage_key = f.readline().strip().split()[0]
-
-    vantage_api_conn = AlphaVantageAPI(v_key)
-    syms, desc = vantage_api_conn.get_nyse_symbols(data_loc)
-
-    data = {}
-
-    for s in syms:
-        l = vantage_api_conn.get_ema(s, period='13')
-        sh = vantage_api_conn.get_ema(s, period='48')
-        data[s] = {}
-        data[s]['long_ema'] = l
-        data[s]['short_ema'] = sh
+    with open(stock_sym_loc, 'r') as f:
+        for line in f.readlines():
+            sym = line.strip().split('\t')[0]
+            try:
+                data = si.get_data(sym)
+                # get 13 and 48.5 day moving average
+                x=1
+            except AssertionError:
+                print('No data found')
 
 if __name__ == '__main__':
     main()
--- utilities/get_all_data.py
+++
@@ -1,3 +0,0 @@
-import yahoo_fin.stock_info as yf
-data = yf.get_quote_table('MSFT')
-x=1
